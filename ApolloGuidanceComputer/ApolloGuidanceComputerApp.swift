55704c8b13d65b718e40db421da2774fb2c1019b
Nicholas Pompea <nick.pompea@gmail,com>
2023-04-11 12:50:45 -0600

    Initial Commit

---
+++ ApolloGuidanceComputer/ApolloGuidanceComputerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloGuidanceComputerApp.swift
+//  ApolloGuidanceComputer
+//
+//  Created by Nicholas Pompea on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ApolloGuidanceComputerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApolloGuidanceComputer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ApolloGuidanceComputer
+//
+//  Created by Nicholas Pompea on 4/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
